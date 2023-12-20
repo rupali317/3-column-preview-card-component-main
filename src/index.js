80e2370d04f8e334e75b1b85a91de85d4109dd6b
PriyankaRC16 <rupali317@gmail.com>
2023-12-20 19:25:27 +0800

    Removing unnecessary code - like deleting the defaults

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('footer'));
 root.render(
   <React.StrictMode>
     
