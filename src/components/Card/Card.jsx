35dcea21a0384d24b29710e50395d9681cc7d4b3
PriyankaRC16 <rupali317@gmail.com>
2023-12-22 18:33:31 +0800

    Applying scss concepts

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import {Button} from "../Button/index.js";
 
 export const Card = (props) => (
   <div className={props.className}>
-    <div>
+    <div className="card__readonly-content">
       <img src={props.src} alt={props.altText} width="64" height="40"></img>
       <h1>{props.title}</h1>
       <p>
