da6dfb89a789fdcffb7ae002f9f2a28b071be30d
PriyankaRC16 <rupali317@gmail.com>
2023-12-23 19:21:31 +0800

    Using data from a separate data file. Concise code

--- src/components/GroupCard/GroupCard.jsx
+++ src/components/GroupCard/GroupCard.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 import { Card } from "../Card";
-//import { CardData } from "../../cardData.js";  How best to use it?
+import { CardData } from "../../cardData.js";
+
+const CardComponents = CardData.map((data) => (
+  <Card
+    src={data.src}
+    altText={data.altText}
+    title={data.title}
+    description={data.description}
+    className={data.className}
+  />
+));
 
 export const GroupCard = () => (
-  <div className="group-card">
-    <Card
-      src="/images/icon-sedans.svg"
-      altText="Logo for sedans"
-      title="Sedans"
-      description="Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip."
-      className="card card--sedan"
-    />
-    <Card
-      src="/images/icon-suvs.svg"
-      altText="Logo for SUVs"
-      title="SUVs"
-      description="Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures."
-      className="card card--suvs"
-    />
-    <Card
-      src="/images/icon-luxury.svg"
-      altText="Logo for luxury"
-      title="Luxury"
-      description="Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style."
-      className="card card--luxury"
-    />
-  </div>
+  <div className="group-card">{CardComponents}</div>
 );
