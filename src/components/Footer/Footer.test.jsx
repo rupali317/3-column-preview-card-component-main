78df5ae827a15b0a49d1df30f6989aee09f9cad5
PriyankaRC16 <rupali317@gmail.com>
2023-12-25 15:08:54 +0800

    removing unused functions

--- src/components/Footer/Footer.test.jsx
+++ src/components/Footer/Footer.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Footer } from "./Footer";
 
 describe("Footer component", () => {
